72572a839656da4fbf92b6ca985f5cb27ab7c009
unknown <milen.m.hinkov@gmail.com>
2022-03-15 12:12:42 +0200

    Clean up project.

--- commons/src/main/java/org/example/commons/repos/TableTennisEventEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.commons.repos;
-
-import org.example.commons.entities.TableTennisEventEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TableTennisEventEntityRepository extends JpaRepository<TableTennisEventEntity, Long> {
-}
